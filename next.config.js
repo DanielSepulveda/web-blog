89828d4c3d0b575a5aa2885cc456bbdb80958b84
Daniel Sepulveda <danielgsepulvedaa@gmail.com>
2020-05-10 00:46:40 -0500

    Create Page Header

--- next.config.js
+++ next.config.js
@@ -1,5 +1,17 @@
-require('dotenv').config();
+/* eslint-disable no-param-reassign */
+require('dotenv').config()
 
 module.exports = {
   env: {},
-};
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      issuer: {
+        test: /\.(js|ts)x?$/,
+      },
+      use: ['@svgr/webpack'],
+    })
+
+    return config
+  },
+}
