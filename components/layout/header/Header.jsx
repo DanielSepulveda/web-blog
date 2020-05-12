98a9de87e4ee3030af81c0cd25d0498baf923577
Daniel Sepulveda <danielgsepulvedaa@gmail.com>
2020-05-11 21:50:10 -0500

    Header redesign && blog card

--- components/layout/header/Header.jsx
+++ components/layout/header/Header.jsx
@@ -1,43 +1,45 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
 import Link from 'next/link'
+import SearchIcon from 'public/static/svg/icon-search.svg'
 import Container from '../container'
-import SearchIcon from '../../../assets/svg/icon-search.svg'
 
 const Header = () => {
   return (
     <header>
       <Container>
-        <div className="text-center h-24 flex justify-center items-center">
-          <h1 className="text-3xl tracking-wider font-thin uppercase">Blogging</h1>
-        </div>
-      </Container>
-      <Container>
-        <div className="border-t border-b border-black">
-          <div className="flex h-12 px-8">
+        <nav>
+          <div className="flex h-24 items-center border-b border-black">
+            <div className="flex-initial mr-12">
+              <Link href="/">
+                <a className="text-2xl tracking-wider font-thin uppercase">Blogging</a>
+              </Link>
+            </div>
             <div className="flex-1">
               <ul className="flex h-full text-xs font-bold tracking-wider uppercase">
                 <li>
-                  <div className="flex h-full justify-center items-center">
-                    <Link href="/">
-                      <a className="hover:text-gray-700">Home</a>
-                    </Link>
-                  </div>
-                </li>
-                <li className="ml-8">
                   <div className="flex h-full justify-center items-center">
                     <Link href="/">
                       <a className="hover:text-gray-700">About Us</a>
                     </Link>
                   </div>
                 </li>
+                {/* <li className="ml-8">
+                  <div className="flex h-full justify-center items-center">
+                    <Link href="/">
+                      <a className="hover:text-gray-700">About Us</a>
+                    </Link>
+                  </div>
+                </li> */}
               </ul>
             </div>
-            <div className="flex justify-center items-center cursor-pointer">
-              <SearchIcon />
+            <div className="flex-initial ml-12 flex justify-end">
+              <div className="cursor-pointer">
+                <img src={SearchIcon} alt="Search Icon" />
+              </div>
             </div>
           </div>
-        </div>
+        </nav>
       </Container>
     </header>
   )
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,9 @@
+/* eslint-disable global-require */
 import React from 'react'
 import Head from 'next/head'
-import Layout, { Container } from '../components/layout'
+import Layout, { Container } from 'components/layout'
+import covidImg from 'public/static/images/covid.jpg'
+import studyingImg from 'public/static/images/studying.jpg'
 
 const Home = () => {
   return (
@@ -9,7 +12,50 @@ const Home = () => {
         <title>Blogging - Home</title>
       </Head>
       <Container>
-        <h3>from layout</h3>
+        <div className="flex">
+          <div className="max-w-sm rounded overflow-hidden shadow-lg">
+            <img height="747" className="w-full" src={covidImg} alt="Sunset in the mountains" />
+            <div className="px-6 py-4">
+              <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+              <p className="text-gray-700 text-base">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et
+                perferendis eaque, exercitationem praesentium nihil.
+              </p>
+            </div>
+            <div className="px-6 py-4">
+              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+                #photography
+              </span>
+              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+                #travel
+              </span>
+              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">
+                #winter
+              </span>
+            </div>
+          </div>
+          <div className="max-w-sm rounded overflow-hidden shadow-lg">
+            <img height="747" className="w-full" src={studyingImg} alt="Sunset in the mountains" />
+            <div className="px-6 py-4">
+              <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+              <p className="text-gray-700 text-base">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et
+                perferendis eaque, exercitationem praesentium nihil.
+              </p>
+            </div>
+            <div className="px-6 py-4">
+              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+                #photography
+              </span>
+              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+                #travel
+              </span>
+              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">
+                #winter
+              </span>
+            </div>
+          </div>
+        </div>
       </Container>
     </Layout>
   )
